b62bac48285cf916dbf0b69c0263f1d39cc348ab
Bryan Leow <bryankhleow@gmail.com>
2024-07-31 11:22:29 +0800

    feat: lab 2

--- hello-world/src/main.tsx
+++ hello-world/src/main.tsx
@@ -8,3 +8,32 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+
+// // const p1 =
+// //   React.createElement("p", null, "This is paragraph 1");
+// const p1 = <p>this is paragraph 1</p>;
+//
+// // const button =
+// //   React.createElement("button",{class: "myButton"} , "this is the button");
+// const button = <button className="myButton">this is the button</button>;
+//
+// // const myList =
+// //   React.createElement("ul",{class: "myButton"} , [
+// //     React.createElement("li", null, "first"),
+// //     React.createElement("li", null, "second"),
+// //     React.createElement("li", null, "third")
+// //   ]);
+// const myList =
+//   <ul>
+//     <li>first</li>
+//     <li>second</li>
+//     <li>third</li>
+//   </ul>;
+//
+// // const myDiv =
+// //   React.createElement("div", null, [p1,button,myList]);
+// const myDiv = <div>{p1}{button}{myList}</div>;
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myDiv);
