bff4808691e57b6381606f9062a30580418b41bd
Bryan Leow <bryankhleow@gmail.com>
2024-07-31 16:26:15 +0800

    feat: lab 7

--- hello-world/src/components/Song/SongList.tsx
+++ hello-world/src/components/Song/SongList.tsx
@@ -3,34 +3,48 @@ import {useState} from "react";
 const Song = (props) => {
   return (<li>
     {props.song.title} by {props.song.artist} has {props.song.votes} votes.
-    <button onClick={() => props.recordVote()}>Vote for this song</button>
+    <button onClick={() => props.recordVote(props.id)}>Vote for this song</button>
   </li>);
 }
 
 const SongList = () => {
 
-  let [song1, setSong1] = useState({title : 'Last thing on my mind', artist: 'Steps', votes: 0});
-  let [song2, setSong2] = useState({title : 'If you\'re over me', artist: 'Years and years', votes: 0});
+  const initialSongs = [
+    {title : 'Last thing on my mind', artist: 'steps', votes : 0},
+    {title : 'If you\'re over me', artist: 'Years and years', votes : 0},
+    {title : 'Top of the world', artist: 'Carpenters', votes: 0},
+    {title: 'Sometimes', artist: 'Erasure', votes : 0}
+  ];
 
-  const changeSong = () => {
-    setSong1({...song1, title : 'Something in your eyes'});
-    setSong2({...song2, title : 'King' })
+  let [showAll, setShowAll] = useState(true);
+  let [songs, setSongs] = useState(initialSongs);
+
+  const addVote = (id) => {
+    const newSongs = songs.map( (song,idx) => idx == id ? {...song, votes: song.votes + 1} : song);
+    setSongs(newSongs);
   }
 
-  const addVote1 = () => {
-    setSong1({...song1, votes: song1.votes+1});
-  }
-  const addVote2 = () => {
-    setSong2({...song2, votes: song2.votes+1});
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
   }
 
   return (
     <div>
+      <h2>Your favourite songs are:</h2>
       <ul>
-        <Song song={song1} recordVote={addVote1}/>
-        <Song song={song2} recordVote={addVote2} />
+        {showAll &&
+          songs.map((song, index) =>
+            <Song key={index} song={song} recordVote={addVote} id={index}/>)
+        }
+
+        {!showAll &&
+          songs.filter(song => song.votes >= 2).map((song, index) =>
+            <Song key={index} song={song} recordVote={addVote} id={index}/>)
+        }
+
       </ul>
-      <button onClick={changeSong}>Change songs</button>
+      <h3>Currently showing {showAll ? 'all' : '2 or more rated'} songs</h3>
+      <button onClick={toggleShowAll}>Show {showAll ? 'only songs with 2 or more votes' : 'all songs'}</button>
     </div>
   );
 };
