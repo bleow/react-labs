8d1c2698f21340c5dfe8518fd8f39c857cd50ac7
Bryan Leow <bryankhleow@gmail.com>
2024-08-01 12:12:55 +0800

    feat: firstproject > lab 10

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -5,36 +5,24 @@ import './App.css'
 import Greet from './components/Greeting/Greeting'
 import SongList from "./components/Song/SongList.tsx";
 import TaskManager from "./components/TaskManager/TaskManager.tsx";
+import {Link, Route, BrowserRouter as Router, Routes} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/*<Greet name="mat" age={"21"}></Greet>*/}
-      {/*<SongList></SongList>*/}
-      <TaskManager></TaskManager>
-      {/*<div>*/}
-      {/*  <a href="https://vitejs.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-      {/*<div className="card">*/}
-      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
-      {/*    count is {count}*/}
-      {/*  </button>*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to test HMR*/}
-      {/*  </p>*/}
-      {/*</div>*/}
-      {/*<p className="read-the-docs">*/}
-      {/*  Click on the Vite and React logos to learn more*/}
-      {/*</p>*/}
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">Song List</Link></li>
+          <li><Link to="/contact">Task Manager</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Greet name="mat" age={"21"}></Greet>} />
+        <Route path="/about" element={<SongList></SongList>} />
+        <Route path="/contact" element={<TaskManager></TaskManager>} />
+      </Routes>
+    </Router>
   )
 }
 
